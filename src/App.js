f3e8b8b58180ec5447f808a97d83ce0554b1bca7
Barsik815 <barsikshot@gmail.com>
2021-01-08 20:50:10 +0200

    gg

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header.jsx'
+import Nav from './Components/Nav.jsx'
+import Profile from "./Components/Profile";
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Nav />
+            <Profile/>
+        </div>
+    );
+}
+export default App;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile =() =>{
+    return (
+        <div>
+            <div className='content'>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Wide_lightning.jpg" width='100%'/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
